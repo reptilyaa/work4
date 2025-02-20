596a92f3dd2394acf0b1468aa933b279a576833b
Logan534857345973 <Loganenemy@gmail.com>
2025-02-20 12:26:13 +0300

    gpgp

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,18 +1,25 @@
-import image from 'pizza1.svg'
 import { Container } from './container';
-    import * as React from 'react';
+import * as React from 'react';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
+
+
 interface Props {
-className?: string;
+    className?: string;
 }
 
 export const Header: React.FC<Props> = ({ className }) => {
-return (
-    <header className={cn('border border-b', className)}>
-    <Container className='flex items-center justify-between py-8'>
-        <h1 className='font-black uppercase 2xl'>next pizza</h1>
-        <img className='/pizza1.svg'alt=''></img>
-    </Container>
-</header>
-);
+    return (
+        <header className={cn('border border-b', className)}>
+            <Container className='flex item-center justify-between py-8'>
+                <div className='flex item-center gap-4'>
+                    <Image src='/pizza1.svg' alt='' width={30} height={35} />
+                    <div>
+                        <h1 className='font-black uppercase text-2xl'>next pizza</h1>
+                        <p>вкусней уже некуда</p>
+                    </div>    
+                </div>
+            </Container>
+        </header>
+    );
 }
